d4f8f01521006d4ddf5874ea24f8da64e9ad54eb
DESKTOP-QKSE2O9\Márk <benem960802@gmail.com>
2022-08-21 17:40:03 +0200

    final touches on error handling and number display

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Calculator with react, using express on the backend for memory save and memory read apis.
+
+Esetleges változtatások:
+    
+
+- lehetőség felhasználók regisztrációjára, belépésére,
+- mentett számok adatbázisban történő tárolása
+
--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -2,9 +2,7 @@ import "./Screen.css"
 
 export default function Screen(props){
     let expression = props.expression
-    if (expression.length > 13){
-        expression = expression.slice(0, 13);
-    }
+
     return (
         <div className="screen">
             <h2>
